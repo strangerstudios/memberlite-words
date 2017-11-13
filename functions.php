7797605693e17cf3f39b9d7e3a612bd9ea93a39f
kimcoleman <kim@strangerstudios.com>
2017-11-13 10:08:09 -0500

    Initial commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Memberlite [Words] Child Theme functions and definitions
+ *
+ * @package Memberlite 2.0
+ * @subpackage Memberlite - Child Theme 1.0
+ */
+ 
+//Enqueue scripts and styles.
+function memberlite_words_enqueue_styles() {
+    wp_enqueue_style( 'memberlite', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'memberlite_words_enqueue_styles' );
+
